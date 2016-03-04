c9028da5dd405440efdef413b06f618687338b0f
CptCreosote <ross410@btinternet.com>
2016-03-04 11:05:14 +0000

    Initial Commit

---
+++ CSVReader/CSV.cs
@@ -0,0 +1,13 @@
+﻿namespace ChickenSoft.CSVReader
+{
+    public class Csv : Parser
+    {
+        public Csv(string csvFileName) : base(csvFileName)
+        {   
+        }
+
+        public Csv(string csvFileName, string csvSchemaFileName) : base(csvFileName, csvSchemaFileName)
+        {
+        }
+    }
+}
---
+++ CSVReader/Interfaces/ICsvSchemaModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ChickenSoft.CSVReader.Interfaces
+{
+    public interface ICsvSchemaModel
+    {
+        int ColumnIndex { get; set; }
+        string ColumnName { get; set; }
+        Type ColumnType { get; set; }
+    }
+}
